01ec1d78374cfa9a95d02a83a074c29e6d78bd6b
rafal.sputowski@gmail.com <RS>
2024-01-17 01:54:17 +0100

    Poprawka 2

--- test.bat
+++ test.bat
@@ -1,1 +1,2 @@
-"C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\TestComplete.exe" "H:\TestComplete\P2\TestProject2.pjs" /run /pv:var1="Var From Outspace"
+SETX TC_RTE_PATH %cd%
+"C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\TestComplete.exe" "TestProject2.pjs" /run /pv:var1="Var From Outspace" /e
